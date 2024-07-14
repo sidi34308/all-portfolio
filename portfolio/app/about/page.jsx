7ad580a18d60e9a0d2391aa0db3677a473759c6a
Sidi Chaikh <sc2104008@qu.edu.qa>
2024-07-14 12:54:35 +0300

    update

--- portfolio/app/about/page.jsx
+++ portfolio/app/about/page.jsx
@@ -1,19 +1,17 @@
 // pages/index.js
-import Navbar from '../components/navbar';
-import Card from '../components/cards';
+import Navbar from "../components/navbar";
+// import Card from "../components/cards";
 
 export default function About() {
   return (
     <div className="max-w-7xl mx-auto px-10 bg-[#121213] text-[#FFFFFF]">
       <Navbar />
       <main className="flex flex-col items-center justify-center min-h-screen py-8">
-        <h1 className="text-2xl font-normal text-center mb-3">
-          
-        </h1>
-    
+        <h1 className="text-2xl font-normal text-center mb-3"></h1>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <img src="/sidi-image.png" className="w-full h-auto "/>
-            <div></div>
+          <img src="/sidi-image.png" className="w-full h-auto " />
+          <div></div>
         </div>
       </main>
     </div>
